fb9aa9dee56aa09fbbf68bdcc7992d8741b3075a
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 23:58:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
-   message: "%{value} is not a valid size" }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)
 end
