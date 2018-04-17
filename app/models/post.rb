9c1ab4addac4d238eeb01a615ed0c49a03880943
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 23:55:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, uniqueness: true, presence: true
+  validates :content, length: { maximum: 250 }
+  validates :summary, length: { maximum: 250 }
 end
