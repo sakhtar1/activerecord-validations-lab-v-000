0b01f185830c6172ed5a9b1df75025237baf0a11
Sharmina Akhtar <sakht318@gmail.com>
2018-04-17 23:57:11 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, uniqueness: true, presence: true
+  validates :title, presence: true
   validates :content, length: { maximum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
