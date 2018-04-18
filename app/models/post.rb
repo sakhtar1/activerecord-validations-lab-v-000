3edd09b881dce63e985f20554b019431fbe7b935
Sharmina Akhtar <sakht318@gmail.com>
2018-04-18 00:14:18 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,9 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, clickbait: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
    message: "%{value} is not a valid category" }
-
-
-  def validate(record)
-    unless record.title "Won't Believe", "Secret", "Top [number]", "Guess"
-      record.errors[:title] << 'Choose names!'
-    end
-  end
+  
 end
+
