63ad7f7ea72684c203d0ac690276564ca02685e9
Sharmina Akhtar <sakht318@gmail.com>
2018-04-18 00:09:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
    message: "%{value} is not a valid category" }
+  validates_with MyValidator
 end
 
 
